476b2a0ae8ae8480fb16db17355072d2f7998cb3
ZRK100 <100259752+ZRK100@users.noreply.github.com>
2022-04-13 16:49:32 +0500

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -19,10 +19,6 @@ export default function Portfolio() {
                         <img src={chat} alt="" />
                         <h3>Chat App</h3>
                     </a>
-                    <a className="item" href="https://ipfs.io/ipfs/QmR6bVPZBU25dnXJDsND8xRRqY9rjghAvU311JoNDgdDPN/?fxhash=ooCipHCrxhkPmLQ34XL62rt6rY2wAkXrWgevgdJpsWianvQ3FjZ">
-                        <img src={nft} alt="" />
-                        <h3>Generative NFT</h3>
-                    </a>
                     <a className="item">
                         <img src={portfolio} alt=""/>
                         <h3> React App </h3>
